a1718c8d0ca375f005244b48250d349e65c6dd91
Pedro de Oliveira <pedro.hrochaoliveira@gmail.com>
2024-01-07 00:06:42 -0300

    Adicionar arquivos de projeto.

---
+++ DevFreela.Core/Entities/ProjectComment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DevFreela.Core.Entities
+{
+    public class ProjectComment : BaseEntity
+    {
+        public ProjectComment(string content, int idProject, int idUser)
+        {
+            this.Content = content;
+            this.IdProject = idProject;
+            this.IdUser = idUser;
+
+            this.CreatedAt = DateTime.Now;
+        }
+
+        public string Content { get; private set; }
+        public int IdProject { get; private set; }
+        public int IdUser { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+    }
+}
---
+++ DevFreela.Core/Entities/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DevFreela.Core.Entities
+{
+    public class Skill : BaseEntity
+    {
+        public Skill(string description)
+        {
+            this.Description = description;
+            this.CreatedAt = DateTime.Now;
+        }
+        
+        public string Description { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+    }
+}
